5f5114f45b665757911572c994a1e487bb8a5baa
Ge Ou <ougejiajia@gmail.com>
2020-02-26 23:00:41 -0800

    Unit2-Assignment

---
+++ Flix-week1/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix-week1
+//
+//  Created by Ge Ou on 2020/2/26.
+//  Copyright © 2020 Ge Ou. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
